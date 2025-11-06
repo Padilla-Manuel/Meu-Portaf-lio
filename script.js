16aa1fd729ea5653c303e99263464faf9750d926
Padilla-Manuel <sanchezpadillamanuelfermin758@gmail.com>
2025-11-05 23:39:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const intro = document.getElementById('intro');
-  if (intro) {
-    intro.addEventListener('animationend', () => {
-      intro.remove();
-    });
-  }
+    const intro = document.getElementById('intro');
+    if (intro) {
+        intro.addEventListener('animationend', () => {
+            intro.remove();
+        });
+    }
 
-  const projectCards = document.querySelectorAll('.cardPro');
-  projectCards.forEach(card => {
-    card.addEventListener('click', () => {
-      card.classList.toggle('flipped');
+    const projectCards = document.querySelectorAll('.cardPro');
+    projectCards.forEach(card => {
+        card.addEventListener('click', () => {
+            card.classList.toggle('flipped');
+        });
     });
-  });
+
+    // Modo Dark
+    const toggle = document.getElementById('darkModeToggle');
+    toggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        
+        // Esta linha foi removida, pois o CSS agora controla a visibilidade dos ícones.
+        // toggle.textContent = document.body.classList.contains('dark-mode') ? '☀️' : 'meiaLua';
+    });
 });
